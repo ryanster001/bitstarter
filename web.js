2d16b0a081ea0e105483c2538ff5d4063f0d7f90
James Bond <g4man_34@hotmail.com>
2013-07-12 07:04:10 +0000

    werw

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var buffer = new Buffer(1000);
-    response.send(buffer.toString('utc-8', fs.readFileSync("index.html")));
+    var buffer = new Buffer(256);
+    response.send(buffer.toString("utc-8", fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
