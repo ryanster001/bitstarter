db70e9b9c7e3694b7bf71f574a092fc6d40efadb
James Bond <g4man_34@hotmail.com>
2013-07-12 07:30:44 +0000

    eoijf

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var buffer = new Buffer(256);
-    response.send(buffer.toString(fs.readFileSync('index.html'), 'utf-8'));
+    var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
